e588920b95915e9ebd6a41f295fb0d712219c60c
Pablo Fontaine <pablofontaine97@gmail.com>
2022-11-24 21:11:34 -0300

    I cleaned 'views.py' from Players App and deleted deprecated functions.

--- blog/players/views.py
+++ blog/players/views.py
@@ -1,27 +1,17 @@
-from django.shortcuts import render
-
-from players.models import TennisPlayer
-from players.forms import TennisPlayerForm
-from django.urls import reverse_lazy
 from django.core.exceptions import ValidationError
-
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from players.forms import TennisPlayerForm
+from players.models import TennisPlayer
 
 # Create your views here.
-
-
-def get_players(request):
-    players = TennisPlayer.objects.all()
-    return players
-
 
 class TennisPlayersView(ListView):
     model = TennisPlayer
     template_name = 'players/players_list.html'
     # paginate_by = 3
-
 
 class TennisPlayerCreateView(CreateView):
     model = TennisPlayer
@@ -64,55 +54,3 @@ class TennisPlayerUpdateView(UpdateView):
     def get_success_url(self):
         player_id = self.kwargs["pk"]
         return reverse_lazy("players:players-detail", kwargs={"pk": player_id})
-
-
-def players_list_add(request):
-
-    form = TennisPlayerForm(
-        request.POST) if request.POST else TennisPlayerForm()
-
-    if request.method == 'GET':
-        return render(
-            request=request,
-            template_name='players/players_add.html',
-            context={
-                'form': form,
-            },
-        )
-    elif request.method == 'POST':
-
-        context_dict = {
-            'message': None,
-            'form': form,
-        }
-
-        if form.is_valid():
-
-            data = form.cleaned_data
-            actually_players = TennisPlayer.objects.filter(
-                name=data['name'],
-            ).count()
-
-            if actually_players == 0:
-                form.save()
-                context_dict.update({
-                    'message': 'Jugador creado con éxito',
-                    'players': get_players(request),
-                },
-                )
-
-                return render(
-                    request=request,
-                    template_name='players/players_list.html',
-                    context=context_dict,
-                )
-
-            else:
-                context_dict.update(
-                    {'message': 'El jugador ya existe, intenta de nuevo.'})
-
-                return render(
-                    request=request,
-                    template_name='players/players_add.html',
-                    context=context_dict,
-                )
