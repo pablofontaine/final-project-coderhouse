e4cf91f12f61eaba55ae431049419a81e16bcf96
Pablo Fontaine <pablofontaine97@gmail.com>
2022-11-25 20:23:02 -0300

    - I finished the authentication system, using the Django models, as well as the user's operability using CRUD. - It is possible to create users, login, logout, change password, update user data and view the user's profile. - The home template requires authentication to display profile editing, and other functions.

--- blog/home/urls.py
+++ blog/home/urls.py
@@ -1,7 +1,24 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy, path
+from django.http import HttpRequest
 from home import views
 
 app_name = 'home'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('accounts/register/', views.register, name="user-register"),
+    path('accounts/register/<int:pk>/update',
+         views.UserUpdateView.as_view(), name='user-update'),
+    path('accounts/register/<int:pk>/detail',
+         views.UserDetailView.as_view(), name='user-detail'),
+    path('accounts/register/<int:pk>/delete',
+         views.UserDeleteView.as_view(), name='user-delete'),
+
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='registration/user_password_change.html',
+        success_url=reverse_lazy('home:password-done'),
+    ), name='password-change'),
+    path('accounts/password_done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='registration/user_password_done.html',
+    ), name='password-done'),
 ]
--- blog/home/views.py
+++ blog/home/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from home.forms import UserRegisterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+
 
 # Create your views here.
 
@@ -9,3 +21,43 @@ def index(request):
         context=dict(),
         template_name="home/index.html",
     )
+
+
+def register(request):
+
+    form = UserRegisterForm(
+        request.POST) if request.POST else UserRegisterForm()
+
+    # if method POST, si el usuario está enviando el formulario
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(request, "¡Usuario creado exitosamente!")
+            return redirect("login")
+
+    # else method GET, si el usario solicita para registrarse
+    return render(
+        request=request,
+        context={"form": form, },
+        template_name="registration/register.html",
+    )
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'registration/user_detail.html'
+    fields = ['fist_name', 'last_name', 'email']
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'registration/user_update.html'
+    fields = ['first_name','last_name','email']
+    message_succes = 'Modificación exitosa'
+
+    def get_success_url(self):
+        user_id = self.kwargs["pk"]
+        return reverse_lazy("players:users-detail", kwargs={"pk": user_id})
+
+class UserDeleteView(LoginRequiredMixin, DeleteView):
+    model = User
+    template_name = 'registration/user_confirm_delete.html'
+    success_url = reverse_lazy('home:index')
