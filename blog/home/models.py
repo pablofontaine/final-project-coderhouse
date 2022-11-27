bd56e4b79b7faea664d5ae1fc7e929dc6ea105a7
Pablo Fontaine <pablofontaine97@gmail.com>
2022-11-27 17:43:03 -0300

    - Fixed bugs from previous version (some views had buttons with wrong urls) - I successfully implemented the creation of news by author based on view. - I added a button in the user's profile to create news. - Finish full CRUD for news handling.

--- blog/home/models.py
+++ blog/home/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class New(models.Model):
+
+    def get_user_id(request):
+        return print(request.user.username)
+
+    title = models.CharField(max_length=140, null=False, blank=False, verbose_name='Título')
+    subtitle = models.CharField(max_length=280, null=False, blank=False, verbose_name='Subtítulo')
+    description = models.TextField(max_length=980, null=False, blank=True, verbose_name='Descripción')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creación')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Última modificación')
+
+    class Meta:
+        unique_together = ()
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Codigo: {self.id} | Autor: {self.author.first_name} {self.author.last_name} | Título: {self.title}'
--- blog/home/urls.py
+++ blog/home/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy, path
-from django.http import HttpRequest
 from home import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.NewListView.as_view(), name="index"),
+    path('news/add/', views.NewCreationView.as_view(), name="new-add"),
+    path('news/<int:pk>/detail', views.NewDetailView.as_view(), name="new-detail"),
+    path('news/<int:pk>/update', views.NewUpdateView.as_view(), name="new-update"),
+    path('news/<int:pk>/delete', views.NewDeleteView.as_view(), name="new-delete"),
     path('accounts/register/', views.register, name="user-register"),
     path('accounts/register/<int:pk>/update',
          views.UserUpdateView.as_view(), name='user-update'),
@@ -13,7 +16,6 @@ urlpatterns = [
          views.UserDetailView.as_view(), name='user-detail'),
     path('accounts/register/<int:pk>/delete',
          views.UserDeleteView.as_view(), name='user-delete'),
-
     path('accounts/password_change/', auth_views.PasswordChangeView.as_view(
         template_name='registration/user_password_change.html',
         success_url=reverse_lazy('home:password-done'),
@@ -21,4 +23,5 @@ urlpatterns = [
     path('accounts/password_done/', auth_views.PasswordChangeDoneView.as_view(
         template_name='registration/user_password_done.html',
     ), name='password-done'),
+
 ]
